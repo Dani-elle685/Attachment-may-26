d1d6c26658b3f064173ec3a8bb298dd1f3a15b16
Dani-elle685 <dannjor2@gmail.com>
2026-05-11 11:58:44 +0300

    added local storage in the script css

--- html-javascript/script.js
+++ html-javascript/script.js
@@ -1,83 +1,89 @@
-const api="https://free.mockerapi.com/mock/a93eb6a0-4a79-4133-a544-35bc8792aab6"
+const api =
+  "https://free.mockerapi.com/mock/a93eb6a0-4a79-4133-a544-35bc8792aab6";
 
-async function getData(){
-    try{
-        // const response = await fetch(url,{
-        //     method:"POST",
-        //     headers:{
-        //         "Content-Type":"application/json"
-        //     }
-        // });
+async function getData() {
+  try {
+    // const response = await fetch(url,{
+    //     method:"POST",
+    //     headers:{
+    //         "Content-Type":"application/json"
+    //     }
+    // });
 
+    const response = await fetch(api, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
+    const data = await response.json();
+    console.log(data);
+    displayData(data.data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
 
-        const response = await fetch(api,{
-            method:"GET",
-            headers:{
-                "Content-Type":"application/json"
-            }
-        });
-        
-        const data = await response.json();
-        console.log(data);
-        displayData(data.data);
-    }catch(error){
-        console.error("Error fetching data:", error);
-    }}
-
-
-function displayData(data){
-    console.log("data->",data);
-    const container = document.getElementById("data-container");
-    container.innerHTML = "";
-    data.employees.forEach((item) => {
-        const itemElement = document.createElement("div");
-        itemElement.classList.add("item");
-        itemElement.innerHTML = `<h2>${item.firstName} ${item.lastName}</h2>
+function displayData(data) {
+  console.log("data->", data);
+  const container = document.getElementById("data-container");
+  container.innerHTML = "";
+  data.employees.forEach((item) => {
+    const itemElement = document.createElement("div");
+    itemElement.classList.add("item");
+    itemElement.innerHTML = `<h2>${item.firstName} ${item.lastName}</h2>
         <p>${item.email}</p>
         <p>${item.phone}</p>
         `;
-        container.appendChild(itemElement);
-    });
+    container.appendChild(itemElement);
+  });
 }
 
-getData();  
+getData();
 
-
-const apires={
-"employees": [
-{ "firstName": "John", 
-     "lastName": "Doe" 
-},
-{ "firstName": "Anna", "lastName": "Smith" },
-{ "firstName": "Peter", "lastName": "Jones" }
-]
-}
-
-
+const apires = {
+  employees: [
+    { firstName: "John", lastName: "Doe" },
+    { firstName: "Anna", lastName: "Smith" },
+    { firstName: "Peter", lastName: "Jones" },
+  ],
+};
 
 document.addEventListener("DOMContentLoaded", function () {
+  const container = document.getElementById("name-container");
+  // Store data
+  localStorage.setItem("apiResponse", JSON.stringify(apires));
+  // Retrieve data
+  const theme = localStorage.getItem("apiResponse");
 
-    const container = document.getElementById("name-container");
+  // Remove a specific item
+  localStorage.removeItem("apiResponse");
 
-    apires.employees.forEach((item) => {
-        console.log("apires response==>", item);
+  // Clear all
+  localStorage.clear();
 
-        const itemElement = document.createElement("div");
+  // Store data
+  sessionStorage.setItem("apiResponse", JSON.stringify(apires));
+  // Retrieve data
+  const sessionData = sessionStorage.getItem("apiResponse");
 
-        itemElement.classList.add("item");
+  // Remove a specific item
+  sessionStorage.removeItem("apiResponse");
 
-        itemElement.innerHTML = `
+  console.log("theme->", theme);
+
+  apires.employees.forEach((item) => {
+    console.log("apires response==>", item);
+
+    const itemElement = document.createElement("div");
+
+    itemElement.classList.add("item");
+
+    itemElement.innerHTML = `
             <h2>${item.firstName} ${item.lastName}</h2>
         `;
 
-        container.appendChild(itemElement);
-    });
-
+    container.appendChild(itemElement);
+  });
 });
-
-
-
-
-
-
